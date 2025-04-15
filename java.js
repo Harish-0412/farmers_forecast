48f44994f652037a13d5bc41ca7975dfc30176fe
Harish K <harish.k2024aiml@sece.ac.in>
2025-04-15 23:22:38 +0530

    Update java.js

---
+++ java.js
@@ -0,0 +1,41 @@
+const container = document.querySelector('.container');
+const registerBtn = document.querySelector('.register-btn');
+const loginBtn = document.querySelector('.login-btn');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+});
+
+// Handle Registration
+document.getElementById('registerForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const username = document.getElementById('regUsername').value;
+    const password = document.getElementById('regPassword').value;
+
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
+
+    alert('Registration successful! You can now login.');
+    container.classList.remove('active'); 
+});
+
+// Handle Login
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+
+    const storedUsername = localStorage.getItem('username');
+    const storedPassword = localStorage.getItem('password');
+
+    if (username === storedUsername && password === storedPassword) {
+        localStorage.setItem('loggedInUser', username); 
+        window.location.href = "farmer.html";
+    }else {
+        alert('Invalid username or password!');
+    }
+});
